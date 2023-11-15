d3d7a70b276e581081fff0c92e5ec53e11047a69
Sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-11-15 22:01:06 +0300

    added agent in use-auth-client file

--- src/use-auth-client.jsx
+++ src/use-auth-client.jsx
@@ -1,7 +1,7 @@
-import { AuthClient } from '@dfinity/auth-client';
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import { AuthClient } from '@dfinity/auth-client'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 const defaultOptions = {
   /**
@@ -22,7 +22,7 @@ const defaultOptions = {
         ? 'https://identity.ic0.app/#authorize'
         : `http://localhost:4943?canisterId=${process.env.CANISTER_ID_INTERNET_IDENTITY}#authorize`,
   },
-};
+}
 
 /**
  *
@@ -32,47 +32,69 @@ const defaultOptions = {
  * @returns
  */
 export const useAuthClient = (options = defaultOptions) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [authClient, setAuthClient] = useState(null);
-  const [identity, setIdentity] = useState(null);
-  const [principal, setPrincipal] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [authClient, setAuthClient] = useState(null)
+  const [identity, setIdentity] = useState(null)
+  const [principal, setPrincipal] = useState(null)
+  const [agent, setAgent] = useState(null)
+  const [backendActor, setBackendActor] = useState(null)
 
   useEffect(() => {
     // Initialize AuthClient
     AuthClient.create(options.createOptions).then(async (client) => {
-      updateClient(client);
-    });
-  }, []);
+      updateClient(client)
+    })
+  }, [])
 
   const login = () => {
     authClient.login({
       ...options.loginOptions,
       onSuccess: () => {
-        updateClient(authClient);
+        updateClient(authClient)
       },
-    });
-  };
+    })
+  }
 
   async function updateClient(client) {
-    const isAuthenticated = await client.isAuthenticated();
-    setIsAuthenticated(isAuthenticated);
+    const isAuthenticated = await client.isAuthenticated()
+    setIsAuthenticated(isAuthenticated)
 
-    const identity = client.getIdentity();
-    setIdentity(identity);
-    console.log('identity :', identity);
+    const identity = client.getIdentity()
+    setIdentity(identity)
+    console.log('identity :', identity)
 
-    const principal = identity.getPrincipal();
+    const principal = identity.getPrincipal()
 
-    console.log('principal :', principal.toString());
-    setPrincipal(principal);
+    console.log('principal :', principal.toString())
+    setPrincipal(principal)
 
-    setAuthClient(client);
+    setAuthClient(client)
 
+    const actor = createActor(canisterId, {
+      agentOptions: {
+        identity,
+      },
+    })
+    setBackendActor(actor)
+    //create an agent
+    const agentInstance = await createAgent({
+      identity,
+      host: HOST,
+    })
+    if (process.env.DFX_NETWORK !== 'ic') {
+      agentInstance.fetchRootKey().catch((err) => {
+        console.warn(
+          'Unable to fetch root key. Check to ensure that your local replica is running',
+        )
+        console.error(err)
+      })
+    }
+    setAgent(agentInstance)
   }
 
   async function logout() {
-    await authClient?.logout();
-    await updateClient(authClient);
+    await authClient?.logout()
+    await updateClient(authClient)
   }
 
   return {
@@ -82,16 +104,16 @@ export const useAuthClient = (options = defaultOptions) => {
     authClient,
     identity,
     principal,
-  };
-};
+  }
+}
 
 /**
  * @type {React.FC}
  */
 export const AuthProvider = ({ children }) => {
-  const auth = useAuthClient();
+  const auth = useAuthClient()
 
-  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
-};
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
+}
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext)
